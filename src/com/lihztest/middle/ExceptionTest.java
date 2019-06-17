9938346799f285fabbaa81a3632ff7faa1d67e18
douya1 <lhzzzs@hotmail.com>
2019-06-17 22:19:43 +0800

    test

--- src/com/lihztest/middle/ExceptionTest.java
+++ src/com/lihztest/middle/ExceptionTest.java
@@ -7,13 +7,37 @@ import java.io.FileNotFoundException;
 public class ExceptionTest {
     public static void main(String[] args) {
 
+        int i = 0;
+        try {
+            i = test2();
+        } catch (Throwable throwable) {
+            throwable.printStackTrace();
+        }
+        System.out.println(i);
+    }
+    static int test1(){
         File file= new File("D://1.txt");
 
         try {
             new FileInputStream(file);
+            System.out.println("1");
+            return 1;
         } catch (FileNotFoundException e) {
             e.printStackTrace();
+            System.out.println("2");
+            return 2;
+        }finally {
+            System.out.println("3");
+            return 3;
         }
-
     }
+
+    static int test2() throws Throwable {
+        File file = new File("D://1.txt");
+
+        new FileInputStream(file);
+        System.out.println("1");
+        return 1;
+    }
+
 }
