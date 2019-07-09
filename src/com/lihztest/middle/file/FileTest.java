5d7cac5c89632210f4dda27269ffb5ab0af6c33d
douya1 <lhzzzs@hotmail.com>
2019-07-09 08:55:50 +0800

    test

--- src/com/lihztest/middle/file/FileTest.java
+++ src/com/lihztest/middle/file/FileTest.java
@@ -4,12 +4,35 @@ import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileOutputStream;
 import java.io.IOException;
+import java.util.List;
 
 public class FileTest {
 
     public static void main(String[] args) {
 
-        copyFile("D:/testFile/1.txt", "D:/testFile/5.txt");
+        //copyFile("D:/testFile/1.txt", "D:/testFile/5.txt");
+        copyFolder("D:/testFile/1", "D:/testFile/2");
+    }
+
+    public static void copyFolder(String srcFolder, String descFolder){
+
+        File f2 = new File(descFolder);
+
+        if(!f2.exists()){
+            f2.mkdirs();
+        }
+        File f1 = new File(srcFolder);
+        File[] l1=f1.listFiles();
+        for(File tmpFile:l1){
+            File newDestFile = new File(descFolder,tmpFile.getName());
+            System.out.println(newDestFile.getAbsolutePath());
+            if(tmpFile.isDirectory()){
+                copyFolder(tmpFile.getAbsolutePath(), newDestFile.getAbsolutePath());
+            }else
+            {
+                copyFile(tmpFile.getAbsolutePath(), newDestFile.getAbsolutePath() );
+            }
+        }
     }
 
     public static void copyFile(String srcFile, String descFile){
---
+++ src/com/lihztest/middle/file/StreamTest4.java
@@ -0,0 +1,42 @@
+package com.lihztest.middle.file;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+
+public class StreamTest4 {
+    public static void main(String[] args) {
+
+    }
+
+    public static void search(File file, String search){
+        if(file.isFile()){
+            if(file.getName().toLowerCase().endsWith(".java")){
+                String fileContent = readFileConent(file);
+                if(fileContent.contains(search)){
+                    System.out.printf("找到目标文件%s, %s",file.getAbsolutePath(), search);
+                }
+            }
+        }
+        if(file.isDirectory()){
+            File[] fileList = file.listFiles();
+            for(File f: fileList){
+                search(f, search);
+            }
+        }
+    }
+
+    private static String readFileConent(File file) {
+
+        try(FileReader fr = new FileReader(file)) {
+            char[] all = new char[(int) file.length()];
+            fr.read(all);
+            return new String(all);
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
