4a716670b06f38cba30f18e744aa11bfb5c3cdad
douya1 <lhzzzs@hotmail.com>
2019-07-15 20:24:45 +0800

    test

--- src/com/lihztest/middle/collection/CollectionsTest.java
+++ src/com/lihztest/middle/collection/CollectionsTest.java
@@ -1,21 +1,67 @@
 package com.lihztest.middle.collection;
 
 import java.util.ArrayList;
+import java.util.Collection;
 import java.util.Collections;
 
 public class CollectionsTest {
     public static void main(String[] args) {
 
 //        reverse();
-        shuffle();
+//        shuffle();
+      //  sort();
+//        swap();
+//        rotate();
+        test();;
     }
 
-    static void shuffle(){
+    static void test(){
+        ArrayList<Integer> list = getList();
+
+        System.out.println(list);
+        int number = 0;
+        for(int i=0;i<1000000;i++){
+            Collections.shuffle(list);
+
+            if(list.get(0).equals(Integer.valueOf("3"))
+                && list.get(1).equals(Integer.valueOf("1"))
+                && list.get(2).equals(Integer.valueOf("2")))
+                number++;
+        }
+        System.out.println("3-1-2 has been appear for "+ number +"number of times");
+    }
+
+    static void rotate(){
+        ArrayList<Integer> list = getList();
+        System.out.println(list);
+        Collections.rotate(list,1);
+        System.out.println(list);
+        Collections.synchronizedList(list);
+    }
+
+    static  void swap(){
+        ArrayList<Integer> list = getList();
+        System.out.println(list);
+        Collections.swap(list,0,8);
+        System.out.println(list);
+    }
+
+    static void sort(){
+        ArrayList<Integer> list ;
+        list = shuffle();
+
+        Collections.sort(list);
+        System.out.println(list);
+
+    }
+
+    static ArrayList<Integer> shuffle(){
 
         ArrayList<Integer> list = getList();
         System.out.println(list);
         Collections.shuffle(list);
         System.out.println(list);
+        return list;
     }
 
     static void reverse(){
--- src/com/lihztest/middle/collection/HashMapTest.java
+++ src/com/lihztest/middle/collection/HashMapTest.java
@@ -2,8 +2,11 @@ package com.lihztest.middle.collection;
 
 import com.lihztest.oot.Hero;
 
+import javax.sound.midi.SoundbankResource;
 import java.util.HashMap;
+import java.util.LinkedHashSet;
 import java.util.LinkedList;
+import java.util.Set;
 
 import static java.lang.System.currentTimeMillis;
 
@@ -11,6 +14,43 @@ public class HashMapTest {
 
     public static void main(String[] args) {
 
+        //reverseMap();
+
+        //fun1();
+        linkedSetTest();
+    }
+
+    static void linkedSetTest(){
+
+        LinkedHashSet<Character> ls = new LinkedHashSet<Character>();
+
+        String str = String.valueOf(Math.PI);
+        System.out.println(str);
+        char [] cs = str.toCharArray();
+        for(char c:cs){
+            if(c>='0' && c<='9')
+                ls.add(c);
+        }
+        System.out.println(ls);
+    }
+
+    static void reverseMap(){
+        HashMap<String,String> map = new HashMap<>();
+        HashMap<String,String> remap = new HashMap<>();
+        map.put("a","1");
+        map.put("b","2");
+        map.put("c","3");
+        System.out.println(map);
+
+        Set<String> keySet = map.keySet();
+
+        for(String s :keySet){
+            remap.put(map.get(s),s);
+        }
+        System.out.println(remap);
+
+    }
+    static void fun1(){
         LinkedList<Hero> list = new LinkedList<Hero>();
         HashMap<String, Hero> map = new HashMap();
 
@@ -30,7 +70,5 @@ public class HashMapTest {
         }
         long l2= System.currentTimeMillis();
         System.out.printf("hero-5555 hava %d, and take %d ms",i, l2-l1);
-
-
     }
 }
